beeb117cb8f93e9a7d9022dc24fdd14e579492db
Stefan Koch <programming@stefan-koch.name>
2018-12-27 08:14:16 +0100

    reload nginx after certificate update

--- ssl/files/certificates-renew.sh
+++ ssl/files/certificates-renew.sh
@@ -2,6 +2,10 @@
 
 certbot renew
 
+# reload nginx after certificates have been renewed
+systemctl reload nginx
+
+# update the certificates in haproxy
 for domaindir in /etc/letsencrypt/live/*/; do
     domain=$(basename $domaindir)
     cat ${domaindir}fullchain.pem ${domaindir}privkey.pem > /etc/haproxy/certs/$domain.pem
